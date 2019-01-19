5932a4c37584b2edbb91d21133470eae331a7925
Pradeep Korivi <korivi@campus.tu-berlin.de>
2019-01-19 12:01:07 +0100

    updated the thresholds and modified the message name

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -142,7 +142,7 @@ class TLDetector(object):
             self.prev_light_loc = None
             return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image_msg, "bgr8")
 
         #Get classification
         # takes arguments as image and classification: one of "opencv", "carla", "dl_based"
